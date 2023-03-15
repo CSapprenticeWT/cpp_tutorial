990c750dc724d1881abeb9e5ecfb0bc5412fb936
CSapprenticeWT <38578892+CSapprenticeWT@users.noreply.github.com>
2023-03-15 22:37:02 +0800

    add new class

---
+++ Student.cpp
@@ -0,0 +1,5 @@
+//
+// Created by 83816 on 2023/3/15.
+//
+
+#include "Student.h"
---
+++ Student.h
@@ -0,0 +1,14 @@
+//
+// Created by 83816 on 2023/3/15.
+//
+
+#ifndef CPP_TUTORIAL_STUDENT_H
+#define CPP_TUTORIAL_STUDENT_H
+
+
+class Student {
+
+};
+
+
+#endif //CPP_TUTORIAL_STUDENT_H
